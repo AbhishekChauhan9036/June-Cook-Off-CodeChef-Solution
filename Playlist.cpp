58a5efd33c4977c310c5ed3e0c47c5d6299413ed
Abhi <abhishekchauhan9036@gmail.com>
2022-06-05 22:02:23 +0530

    commit message

---
+++ Playlist.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+int32_t main(){
+	int turbocpp;
+	cin>>turbocpp;
+	while(turbocpp--){
+	int n,m;
+	cin>>n>>m;
+	int lll=m*3;
+	int answer=n/lll;
+	cout<<answer<<endl;
+	}
+	return 0;
+}
